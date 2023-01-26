d3ed48d4a57766cd740abeb20b177d9c55a4b0e4
Your Name <Solomogokid@gmail.com>
2023-01-25 17:41:35 -0800

    Catch me if you can 2 and Handle multiple successful promises 3

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+// Appends three handlers to function
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
+}
+
+export default handleResponseFromAPI;

0b4c0430512dedfd8b40bd69099111023fc71f82
Your Name <Solomogokid@gmail.com>
2023-02-01 17:48:04 -0800

    Vanilla JS

---
+++ 0x04-TypeScript/js/main.ts
@@ -0,0 +1,23 @@
+interface Student{
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+	fiiirstName: Solomon,
+	lastname: Omisami,
+	age: 24,
+	location: Lagos,
+}
+
+const student2: Student = {
+        fiiirstName: Micheal,
+        lastname: Grace,
+        age: 30,
+        location: USA,
+}
+
+const studentsList = [student1, student2]
+

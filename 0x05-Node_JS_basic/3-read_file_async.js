769181af5e681c91bd94153671460d5c37fbf22a
Solexgreat <solomonGodkid@gmail.com>
2023-03-22 20:55:37 +0000

    Async

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+
+function countStudents(path){
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', (err, data) => {
+            if (err) {
+               throw Error('Cannot load the database');
+            } else {
+                const lines = data.trim().split('\n')
+                const numberofStudents = lines.length - 1
+                console.log(`Number of students: ${numberofStudents}`)
+                let field1 = 0;
+                let field2 = 0;
+                const listOfFirstNameF1 = [];
+                const listOfFirstNameF2 = [];
+                for (let i = 1; i <= numberofStudents; i++){
+                    const line = lines[i].trim('\r').split(',');
+                    for (let j = 0; j < line.length; j++) {
+                        if (line[j] == 'CS') {
+                            listOfFirstNameF1.push(line[0])
+                            field1 += 1;
+                        } else if (line[j] == 'SWE') {
+                            listOfFirstNameF2.push(line[0]);
+                            field2 += 1;
+                        }
+                    }
+                }
+                console.log(`Number of students in CS: ${field1}. List: ${listOfFirstNameF1}`);
+                console.log(`Number of students in SWE: ${field2}. List: ${listOfFirstNameF2}`);
+                resolve();
+            }
+        })
+    })
+}
+
+module.exports = countStudents;
+

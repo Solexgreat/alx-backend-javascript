3b03660c7cb3029dac5cb470e63faf5668fd7452
Solexgreat <solomonGodkid@gmail.com>
2023-03-24 12:49:12 +0100

    Basic Node.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -11,13 +11,12 @@ const app = http.createServer((req, res) => {
         res.end('Hello Holberton School!')
     } else if (req.url === '/students') {
         res.write('This is the list of our students')
-    }
-    students(process.argv[2]).then((data) => {
-        res.write(`Number of students: ${data.numberofStudents}\n`);
-        res.write(`Number of students in CS: ${data.CSlist}\n`);
-        res.write(`Number of students in SWE: ${data.SWElist}`);
-        res.end()
-    }).catch((err) => res.end(err.message))
+        students(process.argv[2]).then((data) => {
+            res.write(`Number of students: ${data.numberofStudents}\n`);
+            res.write(`Number of students in CS: ${data.CSlist}\n`);
+            res.write(`Number of students in SWE: ${data.SWElist}`);
+            res.end() }).catch((err) => res.end(err.message));
+        }
 })
 
 app.listen(port, hostname, () => {

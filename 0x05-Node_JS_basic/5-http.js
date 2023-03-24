5fdbe8f0a1afef5ec7f3af86ec3bd7030b627c8f
Solexgreat <solomonGodkid@gmail.com>
2023-03-24 13:31:23 +0100

    Basic Node.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,9 +2,9 @@ const http = require('http')
 
 const students = require('./3-read_file_async');
 const hostname = '127.0.0.1';
-port = 1245;
+const port = 1245;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
     res.writeHead(200,{'content-Type': 'text/plain'});
 
     if (req.url === '/' ) {
@@ -12,10 +12,10 @@ const app = http.createServer((req, res) => {
     } else if (req.url === '/students') {
         res.write('This is the list of our students')
         students(process.argv[2]).then((data) => {
-            res.write(`Number of students: ${data.numberofStudents - 1}\n`);
+            res.write(`Number of students: ${data.numberofStudents}\n`);
             res.write(`Number of students in CS: ${data.CSlist}\n`);
             res.write(`Number of students in SWE: ${data.SWElist}`);
-            res.end() 
+            res.end();
         }).catch((err) => res.end(err.message));
         }
 })
